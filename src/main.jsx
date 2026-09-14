20a669185a3e5ef122d649d059f01fe0597137e6
김지솔 <zhddl_sol@naver.com>
2026-09-14 16:27:16 +0900

    feat: miss i18n

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './i18n'
 // 구글 OAuth 프로바이더 임포트
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
